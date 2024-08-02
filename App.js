71ffeb67dfbfcd0c76013528c33b6cca5c602578
milindkusahu <milindsahu2000@gmail.com>
2024-08-03 00:19:33 +0530

    react starter code

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+// React.createElement => Object => HTMLElement(render)
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React 🚀!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
