f58de553f2b4d5110907d0efd75c6f3b37628163
milindkusahu <milindsahu2000@gmail.com>
2024-08-03 01:19:32 +0530

    Functional Component

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTMLElement(render)
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello from React 🚀!"
+// React Element
+const heading = (
+  <div>
+    <h1 id="heading">Hello from React using JSX 🚀</h1>
+  </div>
 );
+
+// React Functional Component
+const HeadingComponent = () => {
+  return <h1>This is my Functional Component</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
--- Readme.md
+++ Readme.md
@@ -37,3 +37,18 @@ npm install -D parcel
 - HTTPS
 - Tree Shaking - remove unused code
 - Different dev and production bundles
+
+## React Important Points
+
+### JSX
+
+* JSX and React are two separate things
+* JSX is not HTML in JavaScript
+* JSX is a HTML or XML like syntax
+* JSX transpiled before it reaches the JS -> PARCEL -> Babel
+
+## React Component
+
+* Class Based Component - OLD
+* Functional Component - NEW
+* A function which is returning some piece of JSX is a Functional Component
